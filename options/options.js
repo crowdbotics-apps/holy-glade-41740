9a5d78618801facda7720734f5c7e11345d4c842
Crowdbotics <team@crowdbotics.com>
2023-06-26 14:02:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-glade-41740",
+  url: "https://holy-glade-41740.botics.co",
+  api: "https://holy-glade-41740.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
